7365586fc05ef6f4711d26ab8d8e061589a88a34
mohamedhichri <mohamed.hichri@dataiku.com>
2026-05-08 17:43:55 +0100

    Update install_clis_az_docker_kubectl.sh

--- scripts/shell/install_clis_az_docker_kubectl.sh
+++ scripts/shell/install_clis_az_docker_kubectl.sh
@@ -11,7 +11,6 @@ sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/dock
 sudo dnf install -y docker-ce docker-ce-cli containerd.io && \
 sudo systemctl start docker && \
 sudo systemctl enable docker && \
-sudo usermod -aG docker $USER && \
 docker version && \
 
 # ── 3. kubectl ──────────────────────────────────────────────────
